db15948d46462e233812f2affffd14b0881160d1
Rei <Rei.rodionov@yandex.ru>
2015-12-11 17:40:23 +0300

    Initial Storage

---
+++ StgDocument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Stg
+{
+    public class StgDocument
+    {
+        private string m_Path;
+        private XDocument m_Document;
+        public StgDocument(string path)
+        {
+            m_Path = path;
+        }
+
+        public StgNode Body
+        {
+            get
+            {
+                m_Document = XDocument.Load(m_Path);
+                return new StgNode(m_Document.Element("body"));
+            }
+        }
+    }
+}
